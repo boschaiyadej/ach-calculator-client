9a09114aecdc6d73a1718360b65c11ba35edebba
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-20 07:42:48 +0700

    feat: setup project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="mt-5">Hello, ACH Calculator!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
